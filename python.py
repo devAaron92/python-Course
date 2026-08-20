fdc779783096dcc1744574a45e000616adfa233f
Aaron <[aaronshippey.bus@gmail.com]>
2026-08-20 13:15:50 -0400

    limit

--- python.py
+++ python.py
@@ -1,19 +1,16 @@
-#Please write a program which asks the user to type in an upper limit. 
-#The program then prints out numbers so that each subsequent number is the previous one doubled, 
-#starting from the number 1. That is, the program prints out powers of two in order.
+#Please write a program which asks the user to type in a limit. 
+#The program then calculates the sum of consecutive numbers (1 + 2 + 3 + ...) 
+#until the sum is at least equal to the limit set by the user. 
+#The program should function as follows:
 
-#The execution of the program finishes when the next number to be printed would be greater than the limit set by the user.
-#No numbers greater than the limit should be printed.
-
-## Please change the program from the previous exercise so that the user gets to input 
-#also the base which is multiplied (in the previous program the base was always 2).
-
-upperLmit = int(input("Please enter in an upper Limit: "))
-base = 1
+limit = int(input("Please enter a limit: "))
+var = 0
+sum = 0
 
 
 while True:
-    print(base)
-    base *= 2
-    if base > upperLmit:
-        break
+        var += 1
+        sum += var
+        print(sum)
+        if sum >= limit:
+                break 
