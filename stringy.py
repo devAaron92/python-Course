30e491d554ce18d32dc0f4ca51b0326a96bb96cb
Aaron <[aaronshippey.bus@gmail.com]>
2026-08-24 08:28:18 -0400

    A line of hashes

--- stringy.py
+++ stringy.py
@@ -1,14 +1,6 @@
-#Please write a program which asks the user for a string. 
-#The program then prints out a message based on whether the second character and the second to last character are the same or not. 
-#See the examples below.
+#Please write a program which prints out a line of hash characters, 
+#the width of which is chosen by the user.
 
+lineOfHashes = int(input("Please enter a length for the line of hashes: "))
 
-stringy = input("please enter a string: ")
-
-secondToLast = len(stringy) -2 
-
-if stringy[1] == stringy[secondToLast]:
-    print("The second and the second to last characters are the same")
-
-else:
-    print("The second and the second to last characters are different")
+print(''"#" * lineOfHashes)
