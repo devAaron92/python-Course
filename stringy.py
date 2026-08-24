f7da51f580d1dff59e3711b8487b8768a69cb25c
Aaron <[aaronshippey.bus@gmail.com]>
2026-08-24 18:51:05 -0400

    frame a word

--- stringy.py
+++ stringy.py
@@ -1,19 +1,28 @@
-#Please write a program which asks the user for a string 
-# and then prints it out so that exactly 20 characters are displayed. 
-#If the input is shorter than 20 characters, the beginning of the line is filled in with * characters.
-#You may assume the input string is at most 20 characters long.
+#Please write a program which asks the user for a string and
+#then prints out a frame of * characters with the word in the centre. 
+#The width of the frame should be 30 characters. You may assume the input string will always fit inside the frame.
+#If the length of the input string is an odd number, you may print out the word in either of the two possible centre locations.
 
 
-stringy = input("Please enter a string: ")
-char = "*"
-lengthOfString = len(stringy)
-n = 0
-difference = 20 - lengthOfString
-n = 0
-padding = 0
+stringy = input("please enter a string: ")
+width = (28 - len(stringy)) // 2
+print(30 * "*")
+print("*" +(width * " ") + (stringy) + (" " * width) + "*")
+print(30 * "*")
+#print(" " * difference )
 
-while n < 20 - lengthOfString:
-    n += 1
-    padding = n
+print(width)
 
-print((n * char) + stringy)
+#stringy = input("Please enter a string: ")
+#char = "*"
+#lengthOfString = len(stringy)
+#n = 0
+#difference = 20 - lengthOfString
+#n = 0
+#padding = 0
+
+#while n < 20 - lengthOfString:
+ #   n += 1
+  #  padding = n
+
+#rint((n * char) + stringy)
