aa512bf221362161041c485aecbbee3d7ab5da74
Aaron <[aaronshippey.bus@gmail.com]>
2026-08-26 09:00:44 -0400

    print out all the substrings which end with the last character, from the shortest to the longest.

--- stringy.py
+++ stringy.py
@@ -1,10 +1,11 @@
 #Please write a program which asks the user to type in a string. 
-#The program then prints out all the substrings which begin with the first character, 
-#from the shortest to the longest. Have a look at the example below.
+#The program then prints out all the substrings which end with the last character, from the shortest to the longest. 
+#Have a look at the example below.
 
 stringy = input("Enter a string: ")
 length = len(stringy)
-n = 0
-while n < length:
-    print(stringy[0:n + 1])
-    n += 1
+n = length 
+while n > 0:
+    print(stringy[n-1:length])
+    n -= 1
+   
